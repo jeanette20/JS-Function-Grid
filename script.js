8bd81284f6953b2ec6e0abcd2d45a995d5dadb24
Jeanette Rojas <jrojas@aclanyc.com>
2017-02-27 13:21:48 +0000

    ade some changes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //Write your function calls below this line
+
+setColor(0, 0, "purple");
+setColor(0, 9, "purple");
+setColor(9, 0, "purple");
+setColor(9, 9, "purple");
+
+setColor(2, 3, "black");
+setColor(2, 4, "black");
+setColor(2, 5, "black");
+setColor(2, 6, "black");
+setColor(3, 3, "black");
+setColor(4, 3, "black");
+setColor(5, 3, "black");
+setColor(5, 4, "black");
+setColor(5, 5, "black");
+setColor(5, 6, "black");
